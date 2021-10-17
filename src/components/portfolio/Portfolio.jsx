d47520f8a1b50e7987b6a1b17229e07d1c2623b0
JCJ <jesse.jackson.c@gmail.com>
2021-10-17 16:36:29 -0500

    Created portfolio section, will need to test more layout options

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import './portfolio.scss'
+import React from 'react';
+import './portfolio.scss';
 
 export default function Portfolio() {
     return (
         <div className='portfolio' id='portfolio'>
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li className='active'>Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className='container'>
+                <div className='item'>
+                    <img src='https://via.placeholder.com/150' alt=''></img>
+                    <h3>SAMPLE TITLE</h3>
+                </div>
+                <div className='item'>
+                    <img src='https://via.placeholder.com/150' alt=''></img>
+                    <h3>SAMPLE TITLE</h3>
+                </div>
+                <div className='item'>
+                    <img src='https://via.placeholder.com/150' alt=''></img>
+                    <h3>SAMPLE TITLE</h3>
+                </div>
+                <div className='item'>
+                    <img src='https://via.placeholder.com/150' alt=''></img>
+                    <h3>SAMPLE TITLE</h3>
+                </div>
+                <div className='item'>
+                    <img src='https://via.placeholder.com/150' alt=''></img>
+                    <h3>SAMPLE TITLE</h3>
+                </div>
+                <div className='item'>
+                    <img src='https://via.placeholder.com/150' alt=''></img>
+                    <h3>SAMPLE TITLE</h3>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
