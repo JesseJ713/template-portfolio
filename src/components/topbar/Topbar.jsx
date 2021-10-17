6faee2b78abd95b2f14768134c24b48d9879155e
JCJ <jesse.jackson.c@gmail.com>
2021-10-16 22:14:24 -0500

    Importing/styling logo icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import './topbar.scss'
+import React from 'react';
+import './topbar.scss';
+import { GrMail } from 'react-icons/gr';
+import { FaPhoneAlt } from 'react-icons/fa';
 
 export default function Topbar() {
     return (
         <div className='topbar'>
             <div className='wrapper'>
-                <div className="left">
-                    <a href='#intro' className='logo'>SAMPLE LOGO</a>
+                <div className='left'>
+                    <a href='#intro' className='logo'>
+                        SAMPLE LOGO
+                    </a>
+                    <div className='itemContainer'>
+                        <FaPhoneAlt className='icon' />
+                        <span>5555555555</span>
+                    </div>
+                    <div className='itemContainer'>
+                        <GrMail className='icon' />
+                        <span>jesse.jackson.c@gmail.com</span>
+                    </div>
                 </div>
-                <div className="right"></div>
+                <div className='right'></div>
             </div>
-            
         </div>
-    )
+    );
 }
