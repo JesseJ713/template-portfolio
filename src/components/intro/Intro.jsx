89df54b93ed4aa24329b543d64705be2130d14ce
JCJ <jesse.jackson.c@gmail.com>
2021-10-17 14:15:51 -0500

    Added text to intro page and heavily animated arrow that navigates to portfolio

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import './intro.scss';
+import { FaArrowDown } from 'react-icons/fa';
 
 export default function Intro() {
     return (
         <div className='intro' id='intro'>
             <div className='left'>
-                <div className="imgContainer">
+                <div className='imgContainer'>
                     <img src='assets/thinking-man.png' alt='' />
                 </div>
             </div>
-            <div className='right'></div>
+            <div className='right'>
+                <div className='wrapper'>
+                    <h2>Hi there, I'm</h2>
+                    <h1>Jesse Jackson</h1>
+                    <h3>
+                        Freelance<span></span>
+                    </h3>
+                </div>
+                <a href='#portfolio' alt=''>
+                    <FaArrowDown />
+                </a>
+            </div>
         </div>
     );
 }
