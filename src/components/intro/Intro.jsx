0082fe9fd21bcb93a7027adee017595239c76748
JCJ <jesse.jackson.c@gmail.com>
2021-10-17 14:38:49 -0500

    Animated text through iTyped, animated cursor, useEffect to update span

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import './intro.scss';
+import React, { useEffect, useRef } from 'react';
 import { FaArrowDown } from 'react-icons/fa';
+import { init } from 'ityped';
+import './intro.scss';
 
 export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+        console.log(textRef);
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            strings: ['Full Stack Developer', 'Designer', 'Musician']
+        })
+    }, []);
+
     return (
         <div className='intro' id='intro'>
             <div className='left'>
@@ -15,7 +27,7 @@ export default function Intro() {
                     <h2>Hi there, I'm</h2>
                     <h1>Jesse Jackson</h1>
                     <h3>
-                        Freelance<span></span>
+                        Freelance <span ref={textRef}></span>
                     </h3>
                 </div>
                 <a href='#portfolio' alt=''>
