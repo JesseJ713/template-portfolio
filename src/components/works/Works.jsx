a46c52118a5b7c47fe36ad65cbf367b9e3ba2471
JCJ <jesse.jackson.c@gmail.com>
2021-10-18 01:39:47 -0500

    Template project for slider

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -4,7 +4,25 @@ import './works.scss'
 export default function Works() {
     return (
         <div className='works' id='works'>
-            
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src='https://via.placeholder.com/150' alt="" />
+                                </div>
+                                <h2>SAMPLE TITLE</h2>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis deleniti repellat dicta porro.</p>
+                                <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <img src='https://via.placeholder.com/150' alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
