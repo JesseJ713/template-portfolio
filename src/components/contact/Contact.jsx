a195f40be6e1374190958033f0ac742bb1a60b1d
JCJ <jesse.jackson.c@gmail.com>
2021-10-18 17:36:54 -0500

    Created structure/styling for contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import './contact.scss'
+import React, { useState } from 'react';
+import './contact.scss';
 
 export default function Contact() {
+    const [message, setMessage] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true);
+    };
+
     return (
         <div className='contact' id='contact'>
-            
+            <div className='left'>
+                <img src='https://via.placeholder.com/150' alt='' />
+            </div>
+            <div className='right'>
+                <h2>Contact</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type='text' placeholder='Email' />
+                    <textarea placeholder='Message'></textarea>
+                    <button type='submit'>Send</button>
+                    {message && <span>Message sent!</span>}
+                </form>
+            </div>
         </div>
-    )
+    );
 }
