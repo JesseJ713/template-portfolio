88757e75eca57467b632c2f36ce9ebcb8cb5341e
JCJ <jesse.jackson.c@gmail.com>
2021-10-18 18:16:05 -0500

    Implemented emailjs for functionality of contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,45 @@
 import React, { useState } from 'react';
 import './contact.scss';
+import emailjs from 'emailjs-com';
 
 export default function Contact() {
-    const [message, setMessage] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [emailSent, setEmailSent] = useState(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setMessage(true);
+    const isValidEmail = (email) => {
+        const regex =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(String(email).toLowerCase());
     };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        isValidEmail(email);
+        if (name && email && message) {
+            const serviceId = 'service_e5xeloo';
+            const templateId = 'template_vht4ehh';
+            const userId = 'user_PGezqgpNMtitZtvFSNTuQ';
+            const templateParams = {
+                name,
+                email,
+                message,
+            };
+
+            emailjs
+                .send(serviceId, templateId, templateParams, userId)
+                .then((response) => console.log(response))
+                .then((err) => console.log(err));
+
+            setName('');
+            setEmail('');
+            setMessage('');
+            setEmailSent(true);
+        } else {
+            alert('Please fill in all fields');
+        }
+    }
 
     return (
         <div className='contact' id='contact'>
@@ -17,10 +49,25 @@ export default function Contact() {
             <div className='right'>
                 <h2>Contact</h2>
                 <form onSubmit={handleSubmit}>
-                    <input type='text' placeholder='Email' />
-                    <textarea placeholder='Message'></textarea>
-                    <button type='submit'>Send</button>
-                    {message && <span>Message sent!</span>}
+                    <input
+                        type='text'
+                        placeholder='Your Name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <input
+                        type='email'
+                        placeholder='Your email address'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <textarea
+                        placeholder='Your message'
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    ></textarea>
+                    <button onClick={handleSubmit}>Send Message</button>
+                    {emailSent && <span>Message sent!</span>}
                 </form>
             </div>
         </div>
