6c8018f5c2c20708396e120b58e02ec92251cad3
JCJ <jesse.jackson.c@gmail.com>
2021-10-30 16:38:44 -0500

    Modified constant variables for template

--- src/projectData.js
+++ src/projectData.js
@@ -1,3 +1,5 @@
+// THIS DATA IS DYNAMICALLY GENERATED THROUGHOUT THE PORTFOLIO
+
 export const featuredPortfolio = [
     {
         id: 1,
@@ -187,29 +189,29 @@ export const portfolioSlider = [
 export const testimonialData = [
     {
         id: 1,
-        name: 'Sean Fleming',
-        title: 'Student Success Manager, Coding & Data Analytics & UX/UI & Cybersecurity Boot Camps at UT Austin',
-        img: 'https://media-exp1.licdn.com/dms/image/C4E03AQH1tu3oTfMRXw/profile-displayphoto-shrink_200_200/0/1551624243149?e=1640217600&v=beta&t=nAMZtQ9V0S7vm5IZ_gWiFKNhhKwGUMvKAuXO3afV38g',
-        icon: 'https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg',
-        desc: '...was one of the better students in class, and I know that he has continued coding since he left us. I have no reservations at all about recommending for any organization that he is willing to join.',
-        profile: 'https://www.linkedin.com/in/sean-fleming-atx/',
+        name: 'Sample Name',
+        title: 'Sample Title',
+        img: 'https://via.placeholder.com/150',
+        icon: 'https://via.placeholder.com/150',
+        desc: 'Sample Description',
+        profile: 'https://via.placeholder.com/150',
     },
     {
         id: 2,
-        name: 'Robert Deckard',
-        title: 'Senior Software Engineer at The Home Depot',
-        img: 'https://media-exp1.licdn.com/dms/image/C4E03AQGwTlVB7lzaEA/profile-displayphoto-shrink_200_200/0/1516991610888?e=1640217600&v=beta&t=4l2o_wSHLnd51rtqwoRaEjrXISnTuSJlCouKzf63QSY',
-        icon: 'https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg',
-        desc: '...took a very proactive approach to his learning, he went above and beyond the demands of a very rigorous class and always held himself accountable to learn past what I was teaching...',
-        profile: 'https://www.linkedin.com/in/robert-deckard/',
+        name: 'Sample Name',
+        title: 'Sample Title',
+        img: 'https://via.placeholder.com/150',
+        icon: 'https://via.placeholder.com/150',
+        desc: 'Sample Description',
+        profile: 'https://via.placeholder.com/150',
     },
     {
         id: 3,
-        name: 'Tyler Smith',
-        title: 'MERN Stack Web Developer and Unity C# Game Designer',
-        img: 'https://media-exp1.licdn.com/dms/image/C4E35AQEmu3clrYItFA/profile-framedphoto-shrink_200_200/0/1600203480067?e=1634781600&v=beta&t=D6xo-UZuDFRs23BRbClYrhH3dzV164SqTSDGF03O9nY',
-        icon: 'https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg',
-        desc: '...His ability to remain unfazed by difficult and challenging problems is one of the many reasons I wanted to work with him again...',
-        profile: 'https://www.linkedin.com/in/tyler-smith-atx/',
+        name: 'Sample Name',
+        title: 'Sample Title',
+        img: 'https://via.placeholder.com/150',
+        icon: 'https://via.placeholder.com/150',
+        desc: 'Sample Description',
+        profile: 'https://via.placeholder.com/150',
     },
 ];
